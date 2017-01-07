d9621ff585382f33be2f5b65b7d577b347728b56
Trevor John <trevor@john.tj>
2017-01-07 02:34:37 -0500

    adds Board.js with MOVE constants

---
+++ app/assets/javascripts/board.js
@@ -0,0 +1,14 @@
+class Board {
+  static get MOVE_UP() {
+    return 'up';
+  }
+  static get MOVE_DOWN() {
+    return 'down';
+  }
+  static get MOVE_LEFT() {
+    return 'left';
+  }
+  static get MOVE_RIGHT() {
+    return 'right';
+  }
+}
